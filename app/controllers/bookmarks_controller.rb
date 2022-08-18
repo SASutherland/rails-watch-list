a85b243a4522ecbc368c6f34adba82699da0864f
Shawn Sutherland <shawnsutherland@hotmail.com>
2022-08-18 17:06:31 +0200

    Everything done (except delete)

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,7 +5,19 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list_id = params[:id]
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 
 end
