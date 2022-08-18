30fc15287477f3ef662bb616a724963020188b80
Shawn Sutherland <shawnsutherland@hotmail.com>
2022-08-18 14:57:31 +0200

    Finished models.

---
+++ app/models/movie.rb
@@ -0,0 +1,15 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+
+  before_destroy :check_for_bookmarks
+
+  private
+
+  def check_for_bookmarks
+    return false if :bookmarks != nil
+  end
+
+end
