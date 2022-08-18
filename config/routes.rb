52c9f21ed8b282141ef14404a60c745e3d2c8835
Shawn Sutherland <shawnsutherland@hotmail.com>
2022-08-18 16:54:54 +0200

    Add Bookmark almost working.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :lists, only: [:index, :new, :create, :show] do
+    member do
+      resources :bookmarks, only: [:new, :create]
+    end
+  end
+  resources :bookmarks, only: :destroy
   # Defines the root path route ("/")
   # root "articles#index"
 end
