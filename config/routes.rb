e867870a01250a84b17c4abdea3603a3c64c9729
Shawn Sutherland <shawnsutherland@hotmail.com>
2022-08-19 15:42:09 +0200

    Made changes to buttons.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :new, :create, :show] do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
     member do
       resources :bookmarks, only: [:new, :create]
-    end
+    end # resources :reviews, only: :create
   end
   resources :bookmarks, only: :destroy
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # resources :reviews, only: :destroy
 end
+
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#   resources :lists, only: [:index, :new, :create, :show] do
+#     member do
+#       resources :bookmarks, only: [:new, :create]
+#     end
+#   end
+#   resources :bookmarks, only: :destroy
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# end
